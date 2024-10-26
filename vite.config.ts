e0aff027bc212e234785edbe90a341f66560dc8b
valscholz <val@kiko.homes>
2024-10-27 00:09:37 +0200

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+
+ const env = loadEnv(mode, process.cwd());
+
 	if (mode === 'production' && !process.env.TLDRAW_WORKER_URL) {
 		throw new Error('TLDRAW_WORKER_URL must be set in production')
 	}
@@ -11,7 +14,7 @@ export default defineConfig(({ mode }) => {
 		plugins: [react()],
 		define: {
 			'process.env.TLDRAW_WORKER_URL':
-				process.env.TLDRAW_WORKER_URL ?? '`http://${location.hostname}:5172`',
+				JSON.stringify(process.env.TLDRAW_WORKER_URL ?? '`http://${location.hostname}:5172`'),
 		},
 	}
 })
